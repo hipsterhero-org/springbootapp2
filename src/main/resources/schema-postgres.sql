1187e93f4b2f3a7fc6b844cde343bf0e4f8ac31e
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-04-29 13:12:01 +0200

    Update entity

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS account;
 CREATE TABLE student(id serial PRIMARY KEY, name VARCHAR(255));
-
-CREATE TABLE account(id serial PRIMARY KEY, name VARCHAR(255), account_number VARCHAR(255), uuid VARCHAR(255));
+CREATE TABLE account(id serial PRIMARY KEY, uuid VARCHAR(255), name VARCHAR(255));
