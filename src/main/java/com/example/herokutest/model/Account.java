becae30d706acf45a1f2079e12d1e8842f3ec84c
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-04-29 12:45:16 +0200

    Added account entity

---
+++ src/main/java/com/example/herokutest/model/Account.java
@@ -0,0 +1,70 @@
+package com.example.herokutest.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="account")
+public class Account implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5770718118881140736L;
+
+	@Id
+	@Column
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name="Id")
+	private String uuid;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="account_number")
+	private String accountNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", uuid=" + uuid + ", name=" + name + ", accountNumber=" + accountNumber + "]";
+	}
+}
--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS account;
 CREATE TABLE student(id serial PRIMARY KEY, name VARCHAR(255));
+
+CREATE TABLE account(id serial PRIMARY KEY, name VARCHAR(255), account_number VARCHAR(255), uuid VARCHAR(255));
