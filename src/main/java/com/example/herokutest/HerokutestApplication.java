052c55faa2e2c713a57a9b14ffd3c32eaf800063
Hassan, Omry Emilyo <ohassan@deloitte.it>
2021-04-30 15:39:14 +0200

    removed unused import

--- src/main/java/com/example/herokutest/HerokutestApplication.java
+++ src/main/java/com/example/herokutest/HerokutestApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class HerokutestApplication implements CommandLineRunner {
