2431a6f6366115dd79d0ff6cf9bef4846da0ad9c
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-04-30 14:57:35 +0200

    Test main new branch

--- src/main/java/com/example/herokutest/HerokutestApplication.java
+++ src/main/java/com/example/herokutest/HerokutestApplication.java
@@ -17,7 +17,7 @@ public class HerokutestApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Xplenty app integration ---> STARTED !!!!!!");
+		logger.info("Xplenty app integration ---> STARTED");
 	}
 
 }
